8d30676f7abf1ccc817526433d7397053927d9ea
Rakib mia <rbepari404@gmail.com>
2024-01-10 06:09:14 -0600

    update serivers  api

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const connectDB = async () => {
     console.log("connecting database....");
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sinogwr.mongodb.net/?retryWrites=true&w=majority`;
-    await mongoose.connect(uri, {DbName:"myDB"})
+    await mongoose.connect(uri, {DbName:"car-doctor"})
     console.log("connected database");
 }
 
